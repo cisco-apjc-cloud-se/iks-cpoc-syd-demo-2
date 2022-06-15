837f6cc6c16be28ca4a963dd143ba4555926d787
Richard <richwats@cisco.com>
2022-06-15 14:44:34 +1000

    Unassign

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "terraform-intersight-iks" {
   cluster = {
     name                = var.cluster_name
     ## Tries to deploy before profile is complete - need to deploy twice ...
-    action              = "Deploy" # Unassign, Deploy, Undeploy
+    action              = "Unassign" # Unassign, Deploy, Undeploy
     ## Note: You cannot assign the cluster action as "Deploy" and "wait_for_completion" as TRUE at the same time.
     wait_for_completion = false
     worker_nodes        = var.worker_nodes
